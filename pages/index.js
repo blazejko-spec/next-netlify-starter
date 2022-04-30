ea196fdf48c0fd44012e7948375ac8be3d096309
blazejko-spec <76967656+blazejko-spec@users.noreply.github.com>
2022-04-30 21:36:35 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title><b>twoja matula ssie mi dicka</b></title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
